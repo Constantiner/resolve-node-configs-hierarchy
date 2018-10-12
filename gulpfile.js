57d1634b504ce84f8ed5412d753e48f5525c3dc1
Konstantin Kovalev <constantiner@gmail.com>
2018-10-12 16:59:19 +0200

    Add build process and prepared to publish to NPM

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const pkg = require("./package.json");
+const del = require("del");
+const rollup = require("gulp-better-rollup");
+const SOURCES = "src/*.js";
+const banner = `/**
+* ${pkg.name}
+* ${pkg.description}
+* 
+* @author ${pkg.author}
+* @version v${pkg.version}
+* @link ${pkg.homepage}
+* @license ${pkg.license}
+*/
+
+`;
+
+gulp.task("clean", () => del(["dist", "*.js", "*.mjs", "*.map", "!gulpfile.js"]));
+
+const getSourceFile = () => gulp.src(SOURCES),
+	getDest = () => gulp.dest("."),
+	proceedEs6Modules = () =>
+		getSourceFile()
+			.pipe(
+				rollup(
+					{},
+					{
+						format: "es",
+						banner
+					}
+				)
+			)
+			.pipe(getDest())
+			.pipe(rename({ extname: `.mjs` }))
+			.pipe(getDest());
+
+gulp.task("es6modules", () => proceedEs6Modules());
+
+gulp.task("scripts", gulp.series("es6modules"));
+
+gulp.task("default", gulp.series("clean", "scripts"));
--- src/resolve-node-configs-hierarchy.js
+++ src/resolve-node-configs-hierarchy.js
@@ -33,7 +33,42 @@ const getHierarchicConfigsArray = nodeEnv => ({ path, ext }) =>
 		.filter(Boolean)
 		.map(path => (ext ? `${path}.${ext}` : path));
 
-
+/**
+ * Returns a list of absolute file paths of existing files in the order to apply from first to last
+ * (in order of precedence).
+ *
+ * The base idea is the following https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
+ *
+ * It will list the following files, starting from the bottom.
+ * The first value set (or those already defined in the environment) take precedence:
+ * .env - The Original®
+ * .env.development, .env.test, .env.production - Environment-specific settings.
+ * .env.local - Local overrides. This file is loaded for all environments except test.
+ * .env.development.local, .env.test.local, .env.production.local - Local overrides of environment-specific settings.
+ *
+ * It uses process.env.NODE_ENV for setting environment.
+ *
+ * For test environment it will not list .env.local from this list
+ * since normally you expect tests to produce the same results for everyone.
+ *
+ * It may use any relative path as the base path even with extension.
+ *
+ * for example if you pass "configuration/log4js.json" it will produce the following list for development environment
+ * (if all of these files are exist in file system):
+ * <project_path>/configuration/log4js.development.local.json
+ * <project_path>/configuration/log4js.local.json
+ * <project_path>/configuration/log4js.development.json
+ * <project_path>/configuration/log4js.json
+ *
+ * This utility is asynchronous and returns a promise resolving to file list.
+ *
+ * This utility was inspired by https://github.com/facebook/create-react-app and may contain some chunks of code from it.
+ *
+ * @param {string} file Relative path (from project root) to base file location (for example "config/.env").
+ * @returns {Promise<Array>} A promise resolving to array of absolute file names.
+ *
+ * @see https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
+ */
 const getConfigFiles = acompose(
 	afilter(fileExists),
 	getHierarchicConfigsArray(getEnv()),
@@ -41,40 +76,4 @@ const getConfigFiles = acompose(
 	resolvePath
 );
 
-/**
- * Returns a list of absolute file paths of existing files in the order to apply from first to last 
- * (in order of precedence).
- * 
- * The base idea is the following https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
- * 
- * It will list the following files, starting from the bottom. 
- * The first value set (or those already defined in the environment) take precedence:
- * .env - The Original®
- * .env.development, .env.test, .env.production - Environment-specific settings.
- * .env.local - Local overrides. This file is loaded for all environments except test.
- * .env.development.local, .env.test.local, .env.production.local - Local overrides of environment-specific settings.
- * 
- * It uses process.env.NODE_ENV for setting environment.
- * 
- * For test environment it will not list .env.local from this list 
- * since normally you expect tests to produce the same results for everyone.
- * 
- * It may use any relative path as the base path even with extension.
- * 
- * for example if you pass "configuration/log4js.json" it will produce the following list for development environment 
- * (if all of these files are exist in file system):
- * <project_path>/configuration/log4js.development.local.json
- * <project_path>/configuration/log4js.local.json
- * <project_path>/configuration/log4js.development.json
- * <project_path>/configuration/log4js.json
- * 
- * This utility is asynchronous and returns a promise resolving to file list.
- * 
- * This utility was inspired by https://github.com/facebook/create-react-app and may contain some chunks of code from it.
- * 
- * @param {string} file Relative path (from project root) to base file location (for example "config/.env").
- * @returns {Promise<Array>} A promise resolving to array of absolute file names.
- * 
- * @see https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
- */
 export default getConfigFiles;
