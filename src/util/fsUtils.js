5ee3fd63450e284d8d52e442b109f20c50b40f9b
Konstantin Kovalev <constantiner@gmail.com>
2018-12-20 16:10:49 +0100

    Use fs.access to check file existence

--- src/util/fsUtils.js
+++ src/util/fsUtils.js
@@ -1,12 +1,20 @@
-import { realpath, stat } from "fs";
+import { realpath, access, constants } from "fs";
 import { resolve } from "path";
 import { promisify } from "util";
 
 const realpathAsync = promisify(realpath);
-const statAsync = promisify(stat);
+const accessAsync = promisify(access);
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebook/create-react-app/issues/637
 const resolvedCwd = realpathAsync(process.cwd());
 const resolvePath = async relativePath => resolve(await resolvedCwd, relativePath);
+const fileExists = async file => {
+	try {
+		await accessAsync(file, constants.R_OK);
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
 
-export { statAsync, resolvePath };
+export { fileExists, resolvePath };
