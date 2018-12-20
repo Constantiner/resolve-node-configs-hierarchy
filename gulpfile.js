fb0419916c7151b1ea3c6b6230046d1f70324f7c
Konstantin Kovalev <constantiner@gmail.com>
2018-12-20 13:12:44 +0100

    Include MIT license text into resulting build
    Fix project description
    
    Minor fix build results headers

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,16 @@ const pkg = require("./package.json");
 const del = require("del");
 const rollup = require("gulp-better-rollup");
 const SOURCES = "src/*.js";
-const banner = `/**
-* ${pkg.name}
-* ${pkg.description}
-* 
-* @author ${pkg.author}
-* @version v${pkg.version}
-* @link ${pkg.homepage}
-* @license ${pkg.license}
-*/
+const fs = require("fs");
+let banner = `/**
+ * ${pkg.name}
+ * ${pkg.description}
+ * 
+ * @author ${pkg.author.name} <${pkg.author.email}>
+ * @version v${pkg.version}
+ * @link ${pkg.homepage}
+ * @license ${pkg.license}
+ */
 
 `;
 
@@ -20,8 +21,21 @@ gulp.task("clean", () => del(["dist", "*.js", "*.mjs", "*.map", "!gulpfile.js", 
 
 const getSourceFile = () => gulp.src(SOURCES),
 	getDest = () => gulp.dest("."),
-	proceedEs6Modules = () =>
-		getSourceFile()
+	proceedEs6Modules = () => {
+		const licenseText = fs.readFileSync("./LICENSE", "utf-8");
+		banner = `/**
+ * ${pkg.name}
+ * ${pkg.description}
+ * 
+ * @author ${pkg.author.name} <${pkg.author.email}>
+ * @version v${pkg.version}
+ * @link ${pkg.homepage}
+ * 
+${licenseText.replace(/^/gm, " * ")}
+ */
+
+`;
+		return getSourceFile()
 			.pipe(
 				rollup(
 					{},
@@ -34,6 +48,7 @@ const getSourceFile = () => gulp.src(SOURCES),
 			.pipe(getDest())
 			.pipe(rename({ extname: `.mjs` }))
 			.pipe(getDest());
+	};
 
 gulp.task("es6modules", () => proceedEs6Modules());
 
