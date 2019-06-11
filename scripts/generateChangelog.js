f9d434fa354f9a7e6a6337f6298d2e2dc8c3742b
Konstantin Kovalev <constantiner@gmail.com>
2019-06-11 17:46:44 +0200

    Use dockerized Github Changelog Generator

--- scripts/generateChangelog.js
+++ scripts/generateChangelog.js
@@ -5,11 +5,22 @@ const exec = promisify(childProcessExec);
 const { version } = require("../package.json");
 
 const generateChangelog = async () => {
-	const { stdout, stderr } = await exec(`github_changelog_generator --future-release v${version}`);
+	// const { stdout, stderr } = await exec(`github_changelog_generator --future-release v${version}`);
+	const command = `docker run --rm -v "$(pwd)":/usr/local/src/your-app -e CHANGELOG_GITHUB_TOKEN='${
+		process.env.CHANGELOG_GITHUB_TOKEN
+	}' ferrarimarco/github-changelog-generator --user Constantiner --project resolve-node-configs-hierarchy --future-release v${version}`;
+	const { stdout, stderr } = await exec(command);
 	// eslint-disable-next-line no-console
 	console.log(stdout);
 	// eslint-disable-next-line no-console
 	console.error(stderr);
 };
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on("unhandledRejection", error => {
+	throw error;
+});
 
 generateChangelog();
