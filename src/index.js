071021b48dff1600c35933d4845c8e30d32b05fa
Konstantin Kovalev <constantiner@gmail.com>
2019-08-16 11:10:39 +0200

    Refactor to externalize paths parsing logic

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import { acompose, afilter } from "@constantiner/fun-ctional";
+import { separatePathAndExtension } from "./util/filePathUtil";
 import compose from "./util/compose";
 import { fileExists, fileExistsSync, resolvePath, resolvePathSync } from "./util/fsUtils";
 import { getEnvironment } from "./util/getEnvironment";
-
-const separatePathAndExtension = path => {
-	const pathParts = path.split("/");
-	let fileName = pathParts.pop();
-	let extension = "";
-	const fileNameParts = fileName.split(".");
-	if (fileNameParts.length > 2 || (fileNameParts.length === 2 && fileNameParts[0].length > 0)) {
-		extension = fileNameParts.pop();
-		fileName = fileNameParts.join(".");
-	}
-	const resultingPath = `${pathParts.join("/")}/${fileName}`;
-	return { path: resultingPath, extension };
-};
 
 const canIncludeLocal = (nodeEnvironment, includeTestLocals) => nodeEnvironment !== "test" || includeTestLocals;
 
---
+++ src/util/filePathUtil.js
@@ -0,0 +1,14 @@
+const separatePathAndExtension = path => {
+	const pathParts = path.split("/");
+	let fileName = pathParts.pop();
+	let extension = "";
+	const fileNameParts = fileName.split(".");
+	if (fileNameParts.length > 2 || (fileNameParts.length === 2 && fileNameParts[0].length > 0)) {
+		extension = fileNameParts.pop();
+		fileName = fileNameParts.join(".");
+	}
+	const resultingPath = `${pathParts.join("/")}/${fileName}`;
+	return { path: resultingPath, extension };
+};
+
+export { separatePathAndExtension };
