4bc4572d9fdbef8a979ad3132cddadb6d8413c2c
Konstantin Kovalev <constantiner@gmail.com>
2020-02-20 19:19:01 +0100

    Fix type definitions

--- scripts/types.js
+++ scripts/types.js
@@ -4,6 +4,6 @@ import { resolve } from "path";
 const generatedTypeFilePath = resolve(__dirname, "../types/index.d.ts");
 const content = readFileSync(generatedTypeFilePath, "utf-8");
 
-const newContent = content.replace(/^(\s*)function\s+(.*;)\s*$/gm, "$1export declare function $2");
+const newContent = content.replace(/^(\s*)function\s+(.*;)\s*$/gm, "$1export function $2");
 
 writeFileSync(generatedTypeFilePath, newContent);
